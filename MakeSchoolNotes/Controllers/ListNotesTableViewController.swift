558cfbd6d882532ff1ab6a31ba4546db97265c71
twof <fabiobean2@gmail.com>
2016-06-22 11:38:50 -0700

    Connected tableview to code

--- MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
+++ MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
@@ -10,8 +10,25 @@ import UIKit
 
 class ListNotesTableViewController: UITableViewController {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-  }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // 1
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    // 2
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // 3
+        let cell = tableView.dequeueReusableCellWithIdentifier("listNotesTableViewCell", forIndexPath: indexPath)
+        
+        // 4
+        cell.textLabel?.text = "Yay - it's working!"
+        
+        // 5
+        return cell
+    }
   
 }
